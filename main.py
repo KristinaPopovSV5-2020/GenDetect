d1b60ed078c5a553b24c7ecf5bc56aab60feee0d
Kristina Popov <kristina.popov@sigmoid.rs>
2025-07-10 15:27:37 +0200

    Implement data loader

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from sklearn.metrics import precision_score, accuracy_score, recall_score, f1_score, roc_auc_score
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
+from safe_image_folder import SafeImageFolder
+
+train_transforms = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406],
+                         [0.229, 0.224, 0.225])
+])
+
+val_transforms = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406],
+                         [0.229, 0.224, 0.225])
+])
 
 def calculate_metrics(y_true, y_pred, y_score):
     accuracy = accuracy_score(y_true, y_pred)
@@ -13,3 +31,12 @@ def calculate_metrics(y_true, y_pred, y_score):
     print(f"  Recall   : {recall:.4f}")
     print(f"  F1 Score : {f1:.4f}")
     print(f"  AUC-ROC  : {auc:.4f}")
+
+def get_dataloaders(train_dir, val_dir, batch_size):
+    train_dataset = SafeImageFolder(train_dir, transform=train_transforms)
+    val_dataset = SafeImageFolder(val_dir, transform=val_transforms)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+
+    return train_loader, val_loader
