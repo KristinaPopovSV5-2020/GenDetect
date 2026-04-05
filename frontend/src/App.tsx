7ea5d66e687b43f5e7e59ccda04206398bf96756
Kristina Popov <kristina.popov@sigmoid.rs>
2026-04-05 17:45:48 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, Typography } from "@mui/material";
-import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate"; // ikonica
+import { Box, Button, Typography, Grid } from "@mui/material";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 import "./App.css";
 import Layout from "./components/Layout";
 import { useState } from "react";
@@ -17,48 +17,50 @@ function App() {
 
   return (
     <Layout>
-      <Box sx={{ p: 5,  borderRight: "1px solid rgb(190, 190, 190)"}}>
-        <Box
-          sx={{
-            border: "1px solid #ccc",
-            borderRadius: 2,
-            padding: 2,
-            textAlign: "center",
-            height: "350px",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            flexDirection: "column",
-          }}
-        >
-          {image ? (
-            <img
-              src={image}
-              alt="Uploaded"
-              style={{ width: "100%", height: "300px", objectFit: "contain" }}
-            />
-          ) : (
-            <>
-              <AddPhotoAlternateIcon
-                sx={{ fontSize: 48, mb: 1 }}
+      <Grid container spacing={2} sx={{ p: 5 }}>
+        <Grid size={{ xs:12, sm:6, md: 4}}>
+          <Box
+            sx={{
+              border: "1px solid #ccc",
+              borderRadius: 2,
+              padding: 2,
+              textAlign: "center",
+              height: "350px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          >
+            {image ? (
+              <img
+                src={image}
+                alt="Uploaded"
+                style={{ width: "100%", height: "300px", objectFit: "contain" }}
               />
-              <p>Upload an image</p>
-            </>
-          )}
-          <Button variant="contained" component="label" sx={{ mt: 1, color: "primary.main"}}>
-            Choose Image
-            <input
-              type="file"
-              hidden
-              accept="image/*"
-              onChange={handleImageChange}
-            />
-          </Button>
-        </Box>
-      </Box>
-      <Box sx={{ p: 2 }}>
-        <Typography variant="h3">Detection results</Typography>
-      </Box>
+            ) : (
+              <>
+                <AddPhotoAlternateIcon sx={{ fontSize: 48, mb: 1 }} />
+                <p>Upload an image</p>
+              </>
+            )}
+            <Button variant="contained" component="label" sx={{ mt: 1, color: "primary.main" }}>
+              Choose Image
+              <input
+                type="file"
+                hidden
+                accept="image/*"
+                onChange={handleImageChange}
+              />
+            </Button>
+          </Box>
+        </Grid>
+        <Grid size={{ xs:12, sm:6, md: 4}}>
+          <Box sx={{ p: 2 }}>
+            <Typography variant="h3">Detection results</Typography>
+          </Box>
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
